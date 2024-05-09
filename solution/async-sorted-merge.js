16aff1cca54d0311febf46bdd771eafd2aac90d9
Humberto Mangino <humbertomangino@Humbertos-MacBook-Pro.local>
2024-05-09 11:19:13 -0600

    Challenge solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -4,6 +4,19 @@
 
 module.exports = (logSources, printer) => {
   return new Promise((resolve, reject) => {
-    resolve(console.log("Async sort complete."));
+    // Time complexity : O(n log(n)) because .sort() is using an optimized sorting algorithm and worst case with this native functions is O(n log(n))
+    // Space complexity O(n) because we are creating a new array with .map() and this needs space in memory.
+    const sources = logSources.map((item) => ({ date: item.last.date, msg: item.last.msg })).sort((a, b) => a.date - b.date)
+    const sortedSources = sources.forEach((entry) => {
+      printer.print(entry)
+    })
+    try {
+      resolve(sortedSources)
+      resolve(console.log("Async sort complete."));
+      printer.done()
+    } catch (err) {
+      reject(console.log(err))
+    }
+
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -3,5 +3,15 @@
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  // Time complexity : O(n log(n)) because .sort() is using an optimized sorting algorithm and worst case with this native functions is O(n log(n))
+  // Space complexity O(n) because we are creating a new array with .map() and this needs space in memory.
+  const sources =  logSources.map((item) => ({date: item.last.date, msg: item.last.msg}))
+  .sort((a,b) => a.date - b.date)
+  .forEach((entry) => {
+    printer.print(entry)
+  })
+  printer.done()
+  
+  
   return console.log("Sync sort complete.");
 };
